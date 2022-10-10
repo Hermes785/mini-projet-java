c4ada41c88d00c9179701e3d56735a0f0afc1c8f
Hermes785 <hermesnguefack785@gmail.com>
2022-10-10 22:16:34 +0200

    1ere iteration

---
+++ DynArrayAbstract.java
@@ -0,0 +1,8 @@
+public abstract class DynArrayAbstract {
+
+    protected abstract void add(Object n);
+
+    protected  abstract int size();
+
+    protected  abstract int capacity();
+}
---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+         DynArray  test = new DynArray();
+         Object o1= new Object();
+        Object o2= new Object();
+        Object o3= new Object();
+
+        test.add(o1);
+        System.out.println(01);
+        test.add(02);
+        System.out.println(02);
+        test.add(o3);
+        System.out.println(03);
+
+
+
+    }
+}
